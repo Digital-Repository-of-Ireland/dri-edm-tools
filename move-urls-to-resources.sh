ca3e1245de9ec1c4239fe22e26984cd0f900cf61
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2022-01-10 14:13:34 +0000

    Change format of wikidata and genames URIs

--- move-urls-to-resources.sh
+++ move-urls-to-resources.sh
@@ -10,6 +10,8 @@ then
     exit 1
 fi
 
+# move wikidata dates & spatials, geonames spatials and getty types to rdf:resources.
+# These fields selected based on the sample dataset, may need to add others in future
 sed -i "s/<dc:date>\(https:\/\/www\.wikidata\.org\/wiki\/Q[0-9]*\)<\/dc:date>/<dc:date rdf:resource=\"\1\"\/>/" $1/*.xml
 
 sed -i "s/<dcterms:spatial>\(https:\/\/www\.wikidata\.org\/wiki\/Q[0-9]*\)<\/dcterms:spatial>/<dcterms:spatial rdf:resource=\"\1\"\/>/" $1/*.xml
@@ -18,3 +20,6 @@ sed -i "s/<dcterms:spatial>\(http:\/\/www\.geonames\.org\/[0-9]*\)<\/dcterms:spa
 
 sed -i "s/<dc:type xml:lang=\"eng\">\(http:\/\/vocab\.getty\.edu\/page\/aat\/[0-9]*\)<\/dc:type>/<dc:type rdf:resource=\"\1\"\/>/" $1/*.xml
 
+# Change URIs to the correct formats
+sed -i "s/wikidata\.org\/wiki/wikidata\.org\/entity/" $1/*.xml
+sed -i "s/www\.geonames\.org/sws\.geonames\.org/" $1/*.xml
